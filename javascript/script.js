f7f6a20cd728cf03a41a89470f3da25f8fcd62ba
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-11-28 15:14:47 +0100

    feat: movement of snake

--- javascript/script.js
+++ javascript/script.js
@@ -1,40 +1,46 @@
 const field = document.querySelector('.grid')
-const snake = document.querySelector('.snake');
-const apple = document.querySelector('.food');
 
-let snakeY = 4;
+let snakeY = 10;
 let snakeX = 6;
 
 let appleY = 2;
 let appleX = 1;
 
-
+let velocityY = 0;
+let velocityX = 0;
 
 function startGame(){
-    field.innerHTML += `
-        <div class='food' style="grid-row: ${appleY}; grid-column: ${appleX};"></div>
+    snakeY += velocityY;
+    snakeX += velocityX;
+    field.innerHTML = `
+        <div id='food' style="grid-row: ${appleY}; grid-column: ${appleX};"></div>
+        <div id="snake" style="grid-row: ${snakeY}; grid-column: ${snakeX};"></div>
     `
-    field.innerHTML +=  `
-        <div class="snake" style="grid-row: ${snakeY}; grid-column: ${snakeX};">
-                <div class="eye"></div>
-                <div class="eye"></div>
-            </div>
-    `
+    
 }
 
-startGame();
+let start = setInterval(() =>{
+    startGame()
+}, 200)
 
 document.addEventListener('keydown', function(e){
     let press = e.key
     if(press == 'ArrowDown'){
-        snake.style = 'border-top-left-radius: 5px;border-top-right-radius: 5px; border-bottom-left-radius:50%;border-bottom-right-radius: 50%; flex-direction: row; align-items: flex-end;'
-    
+        
+        velocityY = 1;
+        velocityX = 0;
     }else if(press == 'ArrowUp'){
-        snake.style = "border-top-left-radius: 50%;             border-top-right-radius: 50%; border-bottom-left-radius: 5px; border-bottom-right-radius: 5px; align-items: flex-start; "
+        
+        velocityY = -1;
+        velocityX = 0;
     }else if(press == 'ArrowLeft'){
-        snake.style = "border-top-left-radius: 50%;             border-top-right-radius: 5px; border-bottom-left-radius: 50%; border-bottom-right-radius: 5px; flex-direction: column; align-items: flex-start;"
+        
+        velocityY = 0;
+        velocityX = -1;
     }else if(press == 'ArrowRight'){
-        snake.style = "border-top-left-radius: 5px;             border-top-right-radius: 50%; border-bottom-left-radius: 5px; border-bottom-right-radius: 50%; flex-direction: column; align-items: flex-end;"
+       
+        velocityY = 0;
+        velocityX = 1;
     }
     
  
