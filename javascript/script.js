bef7ae61f4ed77064d5105286157fc4c47fa3b87
TommasoGalistu <165350030+TommasoGalistu@users.noreply.github.com>
2024-11-28 17:19:53 +0100

    fix: gameover after snake touch body

--- javascript/script.js
+++ javascript/script.js
@@ -12,44 +12,65 @@ let velocityX = 0;
 function casualPosition(){
     appleY = Math.floor(Math.random() * 20) + 1;
     appleX = Math.floor(Math.random() * 20) + 1;
+    for (let i = 0; i < snakeBody.length; i++) {
+        if(snakeBody[i][0] == appleX && snakeBody[i][1] == appleY){
+            casualPosition()
+        }
+        
+    }
 }
 
 function startGame(){
+
+    snakeBody.pop([snakeX, snakeY])
     snakeY += velocityY;
     snakeX += velocityX;
     snakeBody.unshift([snakeX, snakeY])
+    for (let i = 1; i < snakeBody.length; i++) {
+        if(snakeBody[0][0] == snakeBody[i][0] && snakeBody[0][1] == snakeBody[i][1]){
+            console.log(snakeBody)
+            gameOver('Il serpente si è mangiato da solo.')
+        }
+    }
     let updateGame = ` 
         <div id='food' style="grid-row: ${appleY}; grid-column: ${appleX};"></div>`
     for (let i = 0; i < snakeBody.length; i++) {
-        updateGame += `
-        <div id="snake" style="grid-row: ${snakeBody[i][1]}; grid-column: ${snakeBody[i][0]};"></div>
-        `
+        if(!i){
+            updateGame += `
+            <div id="snake" style="grid-row: ${snakeBody[i][1]}; grid-column: ${snakeBody[i][0]}; background-color: red;"></div>
+            `
+
+        }else{
+            updateGame += `
+            <div id="snake" style="grid-row: ${snakeBody[i][1]}; grid-column: ${snakeBody[i][0]};"></div>
+            `
+        }
         
     }
+    
     if(snakeY == appleY && snakeX == appleX){
-        snakeBody.unshift([snakeX, snakeY])
-        casualPosition()
+        snakeBody.push([snakeX, snakeY])
+        // casualPosition()
     }
     if(snakeY == 0 || snakeX == 0 || snakeY == 20 || snakeX == 20){
-        clearInterval(start);
-        setTimeout(() =>{
-            gameOver()
-           
-        }, 500)
+        gameOver('Hai toccato le pareti. ')
+        
     }
+    // console.log(snakeBody)
+    
+
+    
     field.innerHTML = updateGame
-    snakeBody.pop([snakeX, snakeY])
+    
     
 }
 
 let start = setInterval(() =>{
     startGame()
-    
-    
 }, 200)
-
 document.addEventListener('keydown', function(e){
     let press = e.key
+    
     if(press == 'ArrowDown'){
         
         velocityY = 1;
@@ -81,13 +102,14 @@ closeModal.addEventListener('click', () =>{
     }, 200)
 })
 
-function gameOver(){
+function gameOver(reason){
+    clearInterval(start);
+    textModal.innerText = reason + "GAME OVER!!!!"
     myModal.style = "display: block;"
     snakeY = 10;
     snakeX = 10;
     snakeBody = [[snakeX, snakeY]];
-    casualPosition()
+    // casualPosition()
     velocityY = 0;
     velocityX = 0;
 }
-
